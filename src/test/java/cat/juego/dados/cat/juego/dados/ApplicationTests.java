415ac960f48c9cb9cca5411532a94095f93e9879
jramma <jrmasip097@gmail.com>
2022-10-21 22:02:20 +0200

    first commit

---
+++ src/test/java/cat/juego/dados/cat/juego/dados/ApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.juego.dados.cat.juego.dados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
